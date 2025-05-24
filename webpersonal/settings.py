770c7fc78ebcc940f43d3641d997ac3939f085fc
delforojas <delfinrojasespina@gmail.com>
2025-05-24 19:57:54 +0200

    Seguridad mejorada: ALLOWED_HOSTS, DEBUG False, admin oculto, actualización de portfolio

--- webpersonal/settings.py
+++ webpersonal/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-z=o8ght64sxwd45_&gfjk15wy*!phgv11sv7!2dd!-chy@*4rw"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = ['delforojas.pythonanywhere.com']
 
-# Application definition  , '127.0.0.1:8000'
+# Application definition  , '127.0.0.1:8000' , delforojas.pythonanywhere.com', '127.0.0.1', 'localhost'
 
 INSTALLED_APPS = [
     "ckeditor",
