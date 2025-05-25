6fc38833184836b211c261e46c35367775cc061a
delforojas <delfinrojasespina@gmail.com>
2025-05-25 04:15:42 +0200

    Actualización final con DEBUG=True y corrección de rutas estáticas

--- webpersonal/settings.py
+++ webpersonal/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-z=o8ght64sxwd45_&gfjk15wy*!phgv11sv7!2dd!-chy@*4rw"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 ALLOWED_HOSTS = ['delforojas.pythonanywhere.com']
