2f4a59511546d0d698cd45cc9a857e8403b050d4
delforojas <delfinrojasespina@gmail.com>
2025-06-02 17:55:01 +0200

    Añadiendo proyecto completo

--- webpersonal/urls.py
+++ webpersonal/urls.py
@@ -37,6 +37,4 @@ urlpatterns = [
     path('contact/', core_views.contact, name="contact"),
     path('admin/', admin.site.urls),
 ] 
-if settings.DEBUG:
-    from django.conf.urls.static import static
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
